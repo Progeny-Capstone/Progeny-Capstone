16b50c892a629823ab4fdcbd5f1aa42fdfa244a1
Mitchel Sandidge <masandidge12@gmail.com>
2020-05-27 15:59:23 -0500

    Added many controllers

---
+++ src/main/java/com/progeny/controllers/EditProfileController.java
@@ -0,0 +1,14 @@
+package com.progeny.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EditProfileController {
+
+    @GetMapping("/profile/edit")
+    public String index() {
+        return "editProfile";
+    }
+
+}
