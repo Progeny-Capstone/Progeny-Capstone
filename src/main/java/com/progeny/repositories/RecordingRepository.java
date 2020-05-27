aebf9cec1be426a4348cf19b2d5e9734ee426ee3
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-05-26 21:29:18 -0500

    Added repositories for recordings and stories. Comments added to repositories

---
+++ src/main/java/com/progeny/repositories/RecordingRepository.java
@@ -0,0 +1,9 @@
+package com.progeny.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecordingRepository { // < What we are dealing with, How it will be identified >
+
+}
--- src/main/java/com/progeny/repositories/UserRepository.java
+++ src/main/java/com/progeny/repositories/UserRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> { // < What we are dealing with, How it will be identified >
+    // We will extend this class and define the type of objects it will be manipulating, as well as the data type of the entity's id.
+
     User findByUsername(String username);
 }
