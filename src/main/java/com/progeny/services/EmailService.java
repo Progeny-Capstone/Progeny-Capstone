9312f0b27dcdcc127b7e7928606d476ba0f8df0a
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-05-28 14:38:57 -0500

    added email service

--- src/main/java/com/progeny/services/EmailService.java
+++ src/main/java/com/progeny/services/EmailService.java
@@ -1,6 +1,7 @@
 package com.progeny.services;
 
 
+import com.progeny.model.Recording;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
@@ -10,24 +11,24 @@ import org.springframework.stereotype.Service;
 @Service("mailService")
 public class EmailService {
 
-//    public JavaMailSender emailSender;
-//
-//    @Value("${spring.mail.from}")
-//    private String from;
-//
-//    public void prepareAndSend(Post post, String subject, String body) {
-//        SimpleMailMessage msg = new SimpleMailMessage();
-//        msg.setFrom(from);
-//        msg.setTo(post.getUser().getEmail());
-//        msg.setSubject(subject);
-//        msg.setText(body);
-//
-//        try{
-//            this.emailSender.send(msg);
-//        }
-//        catch (MailException ex) {
-//            // simply log it and go on...
-//            System.err.println(ex.getMessage());
-//        }
-//    }
+    public JavaMailSender emailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    public void prepareAndSend(Recording recording, String subject, String body) {
+        SimpleMailMessage msg = new SimpleMailMessage();
+        msg.setFrom(from);
+        msg.setTo(recording.getUser().getEmail());
+        msg.setSubject(subject);
+        msg.setText(body);
+
+        try{
+            this.emailSender.send(msg);
+        }
+        catch (MailException ex) {
+            // simply log it and go on...
+            System.err.println(ex.getMessage());
+        }
+    }
 }
