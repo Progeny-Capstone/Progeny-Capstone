d4b7b24a88fd5332ea967627f1026da072bd7338
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-05-28 11:22:46 -0500

    Custom css file are now linked properly.

--- src/main/java/com/progeny/SecurityConfiguration.java
+++ src/main/java/com/progeny/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,10 +17,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public SecurityConfiguration(UserDetailsLoader usersLoader) {
         this.usersLoader = usersLoader;
     }
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
@@ -27,6 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(passwordEncoder()) // How to encode and verify passwords
         ;
     }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -35,24 +39,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .loginPage("/login")
                 .defaultSuccessUrl("/recordings") // user's home page, it can be any URL
                 .permitAll() // Anyone can go to the login page
+
                 /* Logout configuration */
                 .and()
                 .logout()
                 .logoutSuccessUrl("/login?logout") // append a query string value
+
                 /* Pages that can be viewed without having to log in */
                 .and()
                 .authorizeRequests()
                 .antMatchers("/", "/recordings") // anyone can see the home and the ads pages
                 .permitAll()
+
                 /* Pages that require authentication */
                 .and()
                 .authorizeRequests()
                 .antMatchers(
                         "/recordings/create", // only authenticated users can create ads
-                        "/recordings/{id}/edit" // only authenticated users can edit ads
+                        "/recordings/{id}/edit"// only authenticated users can edit ads
                 )
                 .authenticated()
         ;
     }
-
 }
