c1fda30a070fa882c99ef3c0f6b21f674fbc2b32
Johnny Delgado <johnny.m.delgado12@gmail.com>
2020-05-29 17:02:00 -0500

    added ifame for bibi to have epub show on story page

---
+++ src/main/java/com/progeny/controllers/BibiController.java
@@ -0,0 +1,16 @@
+package com.progeny.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BibiController {
+
+//    @GetMapping("/story/bibi")
+//    public String showStoryBibi(){
+//
+//    }
+
+
+}
