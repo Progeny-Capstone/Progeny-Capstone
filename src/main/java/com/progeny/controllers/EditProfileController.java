9edb0a167cdb5debb11ee8c106338b05aa4d662b
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-06-01 17:19:06 -0500

    Working on edit profile

--- src/main/java/com/progeny/controllers/EditProfileController.java
+++ src/main/java/com/progeny/controllers/EditProfileController.java
@@ -1,14 +1,61 @@
 package com.progeny.controllers;
 
+import com.progeny.model.User;
+import com.progeny.repositories.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class EditProfileController {
 
+
+    // --------- INITIALIZE ------------
+    private UserRepository users;
+    private PasswordEncoder passwordEncoder;
+
+
+    // ------------ CONSTRUCTOR METHOD ---------------
+    // --------- AKA DEPENDENCY INJECTION ------------
+    public EditProfileController(UserRepository users, PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    // --------- Edit USER (GET) ------------
     @GetMapping("/profile/edit")
-    public String index() {
+    public String index(Model model) {
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        model.addAttribute("user", user);
+
         return "users/editProfile";
+    }
+
+
+    // --------- Edit USER (POST)------------
+    @PostMapping("/profile/edit")
+    public String saveUser(@ModelAttribute User user){
+
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+
+//        user.setFirstName();
+//        user.setLastName();
+//        user.setEmail();
+//        user.setProfileImageUrl();
+//        user.setLocation();
+//        user.setBio();
+//        user.setUsername();
+
+        users.save(user);
+
+        return "redirect:/profile";
     }
 
 }
