72e5ef6a4c9669220023b68c0b9e9d2e0f95d6cd
Taryn McKenzie <taryn.m.mckenzie@gmail.com>
2020-06-02 14:55:26 -0500

    Added list of stories to stories

--- src/main/java/com/progeny/controllers/StoriesController.java
+++ src/main/java/com/progeny/controllers/StoriesController.java
@@ -1,13 +1,28 @@
 package com.progeny.controllers;
 
+import com.progeny.repositories.GroupRepository;
+import com.progeny.repositories.RecordingRepository;
+import com.progeny.repositories.StoryRepository;
+import com.progeny.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StoriesController {
 
+    private StoryRepository storyRepo;
+
+    // ------------ CONSTRUCTOR METHOD ---------------
+    // --------- AKA DEPENDENCY INJECTION ------------
+    public StoriesController(StoryRepository storyRepo) {
+        this.storyRepo = storyRepo;
+    }
+
     @GetMapping("/stories")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("stories", storyRepo.findAll()); // Place all the groups on the page
         return "stories";
     }
 
