826f9fbc12ee1906916c24427fb04de711657a2b
Mitchel Sandidge <masandidge12@gmail.com>
2020-06-03 11:02:24 -0500

    Adding filestack to register page for profile photo

--- src/main/resources/static/js/filestack.js
+++ src/main/resources/static/js/filestack.js
@@ -1,38 +1,38 @@
 
 
-// -------------- FILESTACK --------------
-window.addEventListener('DOMContentLoaded', function () {
-
-    // -------------- API KEY (Keys.js) --------------
-    const apikey = fileStackKey;
-    const client = filestack.init(apikey);
-
-    // -------------- UPLOAD PREFERENCES --------------
-    const options = {
-        displayMode: 'inline',
-        container: '#inline',
-        maxFiles: 20,
-        uploadInBackground: false,
-        onUploadDone: (res) => console.log(res),
-
-
-    };
-
-
-
-
-    // -------------- FILE PICKER --------------
-    const picker = client.picker(options);
-
-    // -------------- HTML ELEMENTS --------------
-    const openBtn = document.getElementById('open');
-    const closeBtn = document.getElementById('close');
-
-    // -------------- EVENT LISTENERS --------------
-    openBtn.addEventListener('click', () => picker.open());
-    closeBtn.addEventListener('click', () => picker.close());
-
-});
+// // -------------- FILESTACK --------------
+// window.addEventListener('DOMContentLoaded', function () {
+//
+//     // -------------- API KEY (Keys.js) --------------
+//     const apikey = fileStackKey;
+//     const client = filestack.init(apikey);
+//
+//     // -------------- UPLOAD PREFERENCES --------------
+//     const options = {
+//         displayMode: 'inline',
+//         container: '#inline',
+//         maxFiles: 20,
+//         uploadInBackground: false,
+//         onUploadDone: (res) => console.log(res),
+//
+//
+//     };
+//
+//
+//
+//
+//     // -------------- FILE PICKER --------------
+//     const picker = client.picker(options);
+//
+//     // -------------- HTML ELEMENTS --------------
+//     const openBtn = document.getElementById('open');
+//     const closeBtn = document.getElementById('close');
+//
+//     // -------------- EVENT LISTENERS --------------
+//     openBtn.addEventListener('click', () => picker.open());
+//     closeBtn.addEventListener('click', () => picker.close());
+//
+// });
 
 
 
