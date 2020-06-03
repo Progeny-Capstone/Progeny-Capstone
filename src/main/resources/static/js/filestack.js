29ccee4a124df34fc227b9e180d11cac2701b6ca
Mitchel Sandidge <masandidge12@gmail.com>
2020-06-03 17:33:15 -0500

    Can create a recording and save to database, and working on implementing filestack

--- src/main/resources/static/js/filestack.js
+++ src/main/resources/static/js/filestack.js
@@ -1,49 +1,49 @@
 
 
-// -------------- FILESTACK --------------
-window.addEventListener('DOMContentLoaded', function () {
-
-    // -------------- API KEY (Keys.js) --------------
-    const apikey = fileStackKey;
-    const client = filestack.init(apikey);
-
-    // -------------- UPLOAD PREFERENCES --------------
-    const options = {
-        displayMode: 'inline',
-        container: '#inline',
-        maxFiles: 20,
-        uploadInBackground: false,
-        onUploadDone: function (res){
-            console.log(res);
-
-            console.log(res.filesUploaded[0].url);
-
-            let filestackUrl = `<input type="hidden" name="filestackUrl" value="${res.filesUploaded[0].url}"/>`;
-
-            $("#register-form").append(filestackUrl);
-
-
-        },
-
-
-
-    };
-
-
-
-
-    // -------------- FILE PICKER --------------
-    const picker = client.picker(options);
-
-    // -------------- HTML ELEMENTS --------------
-    const openBtn = document.getElementById('open');
-    const closeBtn = document.getElementById('close');
-
-    // -------------- EVENT LISTENERS --------------
-    openBtn.addEventListener('click', () => picker.open());
-    closeBtn.addEventListener('click', () => picker.close());
-
-});
+// // -------------- FILESTACK --------------
+// window.addEventListener('DOMContentLoaded', function () {
+//
+//     // -------------- API KEY (Keys.js) --------------
+//     const apikey = fileStackKey;
+//     const client = filestack.init(apikey);
+//
+//     // -------------- UPLOAD PREFERENCES --------------
+//     const options = {
+//         displayMode: 'inline',
+//         container: '#inline',
+//         maxFiles: 20,
+//         uploadInBackground: false,
+//         onUploadDone: function (res){
+//             console.log(res);
+//
+//             console.log(res.filesUploaded[0].url);
+//
+//             let filestackUrl = `<input type="hidden" name="filestackUrl" value="${res.filesUploaded[0].url}"/>`;
+//
+//             $("#register-form").append(filestackUrl);
+//
+//
+//         },
+//
+//
+//
+//     };
+//
+//
+//
+//
+//     // -------------- FILE PICKER --------------
+//     const picker = client.picker(options);
+//
+//     // -------------- HTML ELEMENTS --------------
+//     const openBtn = document.getElementById('open');
+//     const closeBtn = document.getElementById('close');
+//
+//     // -------------- EVENT LISTENERS --------------
+//     openBtn.addEventListener('click', () => picker.open());
+//     closeBtn.addEventListener('click', () => picker.close());
+//
+// });
 
 
 
