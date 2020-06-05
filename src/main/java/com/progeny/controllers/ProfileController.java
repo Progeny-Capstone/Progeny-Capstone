81d7dbb0742c17c2d4d297355a813a1120182570
Mitchel Sandidge <masandidge12@gmail.com>
2020-06-05 14:59:16 -0500

    Added audio player, and trying show recordings on profile

--- src/main/java/com/progeny/controllers/ProfileController.java
+++ src/main/java/com/progeny/controllers/ProfileController.java
@@ -1,5 +1,6 @@
 package com.progeny.controllers;
 
+import com.progeny.model.Recording;
 import com.progeny.model.User;
 import com.progeny.repositories.GroupRepository;
 import com.progeny.repositories.RecordingRepository;
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class ProfileController {
@@ -38,8 +42,30 @@ public class ProfileController {
         model.addAttribute("friends", currentUser.getFriends()); // Show a list of users attached to current user
 
 
+
         User user = new User(currentUser); // 2. set new user to user ^
+
+
+        System.out.println(user.getFirstName());
+
+
+        if(user.getRecordings() == null){ // if there is no friends list -->
+
+            List<Recording> userRecordings = new ArrayList<>(); // 1. Make a new friends list
+            user.setRecordings(userRecordings); // 2. give the new friends list to User
+
+        }
+
+        long currentUserId = user.getId();
+        System.out.println(currentUserId);
+
+
+
+
+
         model.addAttribute("recordings", user.getRecordings());// 3. Show current recordings for user on profile page
+
+        System.out.println(user.getRecordings());
 
 
         model.addAttribute("groups", groupsRepo.findAll()); // Place all the groups on the page
